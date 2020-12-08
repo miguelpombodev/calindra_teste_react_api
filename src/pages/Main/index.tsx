3e06b0414345238a1b552ee22d2ed2222217875c
Miguel Pombo <mp96dev@gmail.com>
2020-12-08 20:02:52 -0300

    submitting form request successfully

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,39 @@
+import React, { } from 'react';
+import { useForm } from 'react-hook-form';
+// import * as Yup from 'yup'
+
+import api from '../../services/api';
+
+import { SearchForm } from './styles'
+
+interface FormProps {
+    search: string;
+}
+
+const Main: React.FC = () => {
+    const { register, handleSubmit, errors } = useForm();
+
+    const submitSearchHandler = async (data: FormProps) => {
+        try {
+            const { search } = data;
+            console.log(search)
+            const apiResponse = await api.get(`/autocomplete/${search}`);
+
+            console.log(apiResponse);
+        } catch (error) {
+            // console.log(error);
+        }
+    }
+
+    return (
+        <>
+            <SearchForm onSubmit={handleSubmit(submitSearchHandler)}>
+                <input ref={register({ required: true })} name="search" type="text" />
+                {errors.search && <span>Esse campo precisa ser preenchido</span>}
+                <button>Buscar</button>
+            </SearchForm>
+        </>
+    )
+}
+
+export default Main;
